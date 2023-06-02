c451b24e61033a485c3c43968802b41cfc062fde
Pattov <73602815+Pattov@users.noreply.github.com>
2023-06-02 02:29:55 +0200

    Update boton.js

--- js/boton.js
+++ js/boton.js
@@ -4,8 +4,10 @@ const panelJavascript = document.getElementById("editor-js");
 const panelHtml= document.getElementById("editor-html");
 const botonBorrar = document.getElementById('iconoBorrar');
 const iconoBorrar = botonBorrar.querySelector("i");
-
+//2023 pendiente comentar función iconoCopiar
 function Icono() {
+  
+  // C A M B I A R   D I S E Ñ O
   if (iconoElemento.classList.contains("bx-copy")) {
     iconoElemento.classList.remove("bx-copy");
     iconoElemento.classList.add("bx-check");
@@ -14,9 +16,11 @@ function Icono() {
     setTimeout(() => {
       iconoElemento.classList.remove("bx-check");
       iconoElemento.classList.add("bx-copy");
-    }, 1500);
+    }, 1500); // Se repite cada 1500 milisegundos = 1.5 segundos 
   }
-
+  
+  // G U A R D A R   E N   E L   P O R T A P A P E L E S 
+  //Almacenar el div con el contenido de panel JavaScript.
   const content = panelJavascript.getElementsByClassName('cm-content')[0].innerText;
   
   // Crear un elemento de entrada de texto oculto
@@ -35,7 +39,7 @@ function Icono() {
   // Se Elimina el elemento de entrada de texto del DOM
   document.body.removeChild(textArea);
 }
-
+// 2023
 function IconoBorrar() {
   //Se quita el icono y cambiamos el select
   document.getElementById('ejemplos').options[0].selected = true;
