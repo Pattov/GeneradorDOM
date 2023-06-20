11ca2c9d746162511ffe3b63d2a76a30dba0e313
Patriciatvr <patricia.tvr6@gmail.com>
2023-06-21 01:17:08 +0200

    diseño guia hecho

--- js/modal.js
+++ js/modal.js
@@ -5,6 +5,13 @@ const modalTest = document.querySelector('.modal.mTest');
 const closeModalGuia = document.querySelector('.cerrar.guia');
 const closeModalTest = document.querySelector('.cerrar.test');
 
+const pag1 = document.getElementById('pag1');
+const pag2 = document.getElementById('pag2');
+const pag3 = document.getElementById('pag3');
+const bola1 = document.getElementById('bola1');
+const bola2 = document.getElementById('bola2');
+const bola3 = document.getElementById('bola3');
+
 openModalTest.addEventListener('click', (e) => {
   e.preventDefault();
   modalTest.classList.add('modal--show');
@@ -24,3 +31,61 @@ closeModalTest.addEventListener('click', (e) => {
     e.preventDefault();
     modalTest.classList.remove('modal--show');
 });
+
+function selectDiv(divNumber) {
+  pag1.classList.remove('out');
+  pag2.classList.remove('out');
+  pag3.classList.remove('out');
+  pag1.classList.remove('in');
+  pag2.classList.remove('in');
+  pag3.classList.remove('in');
+  
+  if(divNumber == 1) {
+      bola1.classList.add('active');
+      bola2.classList.remove('active');
+      bola3.classList.remove('active');
+
+      pag2.classList.add('out');
+      pag3.classList.add('out');
+      setTimeout(() => {
+          pag2.classList.add('ocultar');
+          pag3.classList.add('ocultar');
+          
+          pag1.classList.remove('ocultar');
+          pag1.classList.add('in');
+      }, 500);
+  };
+  if(divNumber == 2) {
+      bola1.classList.remove('active');
+      bola2.classList.add('active');
+      bola3.classList.remove('active');
+
+      pag1.classList.add('out');
+      pag3.classList.add('out');
+      setTimeout(() => {
+          pag1.classList.add('ocultar');
+          pag3.classList.add('ocultar');
+
+          pag2.classList.remove('ocultar');
+          pag2.classList.add('in');
+      }, 500);
+
+  };
+  if(divNumber == 3) {
+      bola1.classList.remove('active');
+      bola2.classList.remove('active');
+      bola3.classList.add('active');
+
+      pag1.classList.add('out');
+      pag2.classList.add('out');
+      setTimeout(() => {
+          pag1.classList.add('ocultar');
+          pag2.classList.add('ocultar');
+          
+          pag3.classList.remove('ocultar');
+          pag3.classList.add('in');
+      }, 500);
+
+  };
+}
+
